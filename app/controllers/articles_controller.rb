b5bfea144f07f66587e46dbc37448b2a1a06b45a
Nikita Antonenkov <krigar1184@gmail.com>
2018-06-06 22:04:10 +0300

    Articles crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,12 +23,23 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
   def index
     @articles = Article.all
   end
 
   def edit
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
